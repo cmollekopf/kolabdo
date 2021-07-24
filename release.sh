e82d0dd86d4a2c809323f90f6260a91129537294
Christian Mollekopf <mollekopf@kolabsys.com>
2021-07-24 11:32:43 +0200

    Always use the same filename for the release

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # ./release.sh v0.1.9
 set -x
-gh release create $1 --notes "" 'build/app/outputs/apk/release/app-release.apk#kolabdo.apk'
+flutter build apk --release
+cp build/app/outputs/apk/release/app-release.apk kolabdo.apk
+gh release create $1 --notes "" 'kolabdo.apk#kolabdo.apk'
