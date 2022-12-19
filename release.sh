c6bf49587084eb2a32d96a14bc93c4af74d70d33
Christian Mollekopf <mollekopf@apheleia-it.ch>
2022-12-19 21:35:07 +0100

    Bump the version again

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# ./release.sh v0.1.9
 set -x
+set -e
+VERSION=$(grep version: pubspec.yaml | sed "s/version: //")
 flutter build apk --release
 cp build/app/outputs/apk/release/app-release.apk kolabdo.apk
-gh release create $1 --notes "" 'kolabdo.apk#kolabdo.apk'
+gh release create "v$VERSION" --notes "New release" "kolabdo.apk#kolabdo.apk"
